5d1b1bd5dc16707f53c519ebbf2efba000ea4272
Li-Xin <mzydj@139.com>
2017-12-04 21:24:19 +0800

    feature:基本测试编写
    由于测试环境无法连接谷歌，更换jquery源为国内CDN源，将谷歌jsapi下载之后更换为本地文件加载。

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -25,29 +25,50 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
+         it('有链接', function() {
+            for(const i of allFeeds) {
+                expect(i.url).toBeDefined();
+                expect(i.url.length).not.toBe(0);
+            }
+         });
 
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
+         it('有名字', function() {
+            for(const i of allFeeds) {
+                expect(i.name).toBeDefined();
+                expect(i.name.length).not.toBe(0);
+            }
+         });
     });
 
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
-
+    describe('The menu', function() {
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
+         it('默认是隐藏的', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
 
          /* TODO:
           * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
           * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
-
+          it('点击之后状态切换', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
+    });
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
-
+    describe('Initial Entries', function() {
         /* TODO:
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -55,11 +76,37 @@ $(function() {
          * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
          * 和异步的 done() 函数。
          */
+         beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+         });
+
+         it('工作正常', function(done) {
+            expect($('.feed').children('.entry-link').length).not.toBe(0);
+            done();
+          });
+     });
 
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
-
+    describe('New Feed Selection', function() {
         /* TODO:
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
+         const newid=1,
+            feed = $('.feed');
+
+         beforeEach(function(done) {
+            loadFeed(newid, function() {
+                done();
+            });
+         });
+
+         // 将新的内容进行加载，对比之前的页面与加载新的页面是否相同
+         it('可以更新', function(done) {
+            expect($('.feed')).not.toBe(feed);
+            done();
+          });
+     });
 }());
