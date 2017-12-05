9d9f60ad07fb181af9bc49264f9001a4bd8fda22
Li-Xin <mzydj@139.com>
2017-12-05 13:58:03 +0800

    style:缩进格式化

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,15 +9,15 @@
 $(function() {
     /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
-    */
+     */
     describe('RSS Feeds', function() {
         /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
          * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
          * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
          * 页面看看会发生什么。
-        */
+         */
         const regularUrl = /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/;
-        
+
         function checkSome(data) {
             expect(data).toBeDefined();
             expect(data.length).not.toBe(0);
@@ -30,28 +30,28 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
-         it('有链接', function() {
-            for(const i of allFeeds) {
+        it('有链接', function() {
+            for (const i of allFeeds) {
                 checkSome(i.url);
             }
-         });
+        });
 
-         // 使用正则表达式确认链接为有效链接
-         it('链接有效', function() {
-            for(const i of allFeeds) {
+        // 使用正则表达式确认链接为有效链接
+        it('链接有效', function() {
+            for (const i of allFeeds) {
                 expect(i.url).toMatch(regularUrl);
             }
-         });
+        });
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
-         it('有名字', function() {
-            for(const i of allFeeds) {
+        it('有名字', function() {
+            for (const i of allFeeds) {
                 expect(i.name).toBeDefined();
                 expect(i.name.length).not.toBe(0);
             }
-         });
+        });
     });
 
 
@@ -61,21 +61,21 @@ $(function() {
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
-         it('默认是隐藏的', function() {
+        it('默认是隐藏的', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
-         });
+        });
 
-         /* TODO:
-          * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
-          * 再次点击的时候是否隐藏。
-          */
-          it('点击之后状态切换', function() {
+        /* TODO:
+         * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
+         * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
+         * 再次点击的时候是否隐藏。
+         */
+        it('点击之后状态切换', function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
+        });
     });
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
     describe('Initial Entries', function() {
@@ -86,14 +86,14 @@ $(function() {
          * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
          * 和异步的 done() 函数。
          */
-         beforeEach(function(done) {
+        beforeEach(function(done) {
             loadFeed(0, done);
-         });
+        });
 
-         it('工作正常', function() {
+        it('工作正常', function() {
             expect($('.feed').children('.entry-link').length).not.toBe(0);
-          });
-     });
+        });
+    });
 
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
     describe('New Feed Selection', function() {
@@ -101,19 +101,19 @@ $(function() {
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
-         const newid=1;
-         let feed;
+        const newid = 1;
+        let feed;
 
-         beforeEach(function(done) {
+        beforeEach(function(done) {
             loadFeed(newid, function() {
                 feed = $('.feed');
                 loadFeed(0, done);
             });
-         });
+        });
 
-         // 将新的内容进行加载，对比之前的页面与加载新的页面是否相同
-         it('可以更新', function() {
+        // 将新的内容进行加载，对比之前的页面与加载新的页面是否相同
+        it('可以更新', function() {
             expect($('.feed')).not.toBe(feed);
-          });
-     });
+        });
+    });
 }());
